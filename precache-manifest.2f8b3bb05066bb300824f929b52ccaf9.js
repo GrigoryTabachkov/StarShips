1bfaea9d35427111136aa06bc5f59e57223935e8
GrigoryTabachkov <tabbartend@gmail.com>
2020-08-31 14:35:05 +0300

    Updates

---
+++ precache-manifest.2f8b3bb05066bb300824f929b52ccaf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d06f58a8126f18dd0ad5c8983e0193",
+    "url": "/StarShips/index.html"
+  },
+  {
+    "revision": "fa874b67b4aea143ba0d",
+    "url": "/StarShips/static/css/main.08b592bb.chunk.css"
+  },
+  {
+    "revision": "81e3935359749b50cf2c",
+    "url": "/StarShips/static/js/2.b5e9993e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/StarShips/static/js/2.b5e9993e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa874b67b4aea143ba0d",
+    "url": "/StarShips/static/js/main.9377b153.chunk.js"
+  },
+  {
+    "revision": "c0f268e17a037004a393",
+    "url": "/StarShips/static/js/runtime-main.22b80a3b.js"
+  }
+]);
